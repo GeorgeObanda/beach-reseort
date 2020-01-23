75aa981b41f4f213050ccda2f2001d7406bf0e01
George Obanda <44008263+GeorgeObanda@users.noreply.github.com>
2020-01-23 11:22:36 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# beach-reseort
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <marquee behavior="scroll" scrollamount="1" style={{ color: "orange" }}>
+        Motto:Education is power: Misson: Be a a qualityt institution
+      </marquee>
+    </div>
+  );
+}
